916a0402319b88d410ddef6038275083692b4390
entcher <49116032+entcher@users.noreply.github.com>
2023-11-30 16:56:59 +0300

    update tests

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import unittest
+
+
 def sum(a, b):
     return a + b
 
 
-print(sum(1, 4))
+class TestSum(unittest.TestCase):
+
+    def test_3(self):
+        self.assertEqual(sum(1, 2), 3)
+
+    def test_4(self):
+        self.assertEqual(sum(2, 2), 4)
+
+    def test_5(self):
+        self.assertEqual(sum(3, 2), 5)
